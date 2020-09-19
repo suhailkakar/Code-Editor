2036ea6442a90f55007e2d017c1fc1df9b70df0b
Suhail Kakar <56798748+suhailkakar@users.noreply.github.com>
2020-09-20 00:30:03 +0530

    Add files via upload

---
+++ build/precache-manifest.83f80605fe55f09e299a43e34a4ed502.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f244afc81c94bdd34f2a0a3498054c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee9a223b83d919bbdec6",
+    "url": "/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "ec4f7dfaefacf0b1e334",
+    "url": "/static/css/main.0fde0f8f.chunk.css"
+  },
+  {
+    "revision": "ee9a223b83d919bbdec6",
+    "url": "/static/js/2.c3b76f88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3b76f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec4f7dfaefacf0b1e334",
+    "url": "/static/js/main.4720fe88.chunk.js"
+  },
+  {
+    "revision": "73d581c7e4c99e5a0dc8",
+    "url": "/static/js/runtime-main.1457abd3.js"
+  }
+]);
